fe8e555d16e849cac7bb0931213be29292fd8e94
erictsai123 <erictsai03@gmail.com>
2020-07-23 17:41:00 -0700

    Updates

---
+++ precache-manifest.1d7c2e53abe0e7e75e46fd13283f2eff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2887aa7326364a076ac5482503c2c982",
+    "url": "/Typer/index.html"
+  },
+  {
+    "revision": "4e4b7f56eab5085cefb4",
+    "url": "/Typer/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "eb86c7d6c139d2baef11",
+    "url": "/Typer/static/css/main.38287d3b.chunk.css"
+  },
+  {
+    "revision": "4e4b7f56eab5085cefb4",
+    "url": "/Typer/static/js/2.6c1444e7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Typer/static/js/2.6c1444e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb86c7d6c139d2baef11",
+    "url": "/Typer/static/js/main.8387191c.chunk.js"
+  },
+  {
+    "revision": "62434349b4cd7437501b",
+    "url": "/Typer/static/js/runtime-main.fe0bba74.js"
+  }
+]);
